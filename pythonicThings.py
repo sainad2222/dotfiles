91158e9090c4e56694c5b03794b35c4c94ae56b3
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2020-09-16 15:14:26 +0530

    Added pythonic things

---
+++ pythonicThings.py
@@ -0,0 +1,34 @@
+# # making pairs in python
+# s = [1,3,0,5,8,5]
+# f = [2,6,9,7,4,9]
+# unsorted = list(zip(s,f))
+# sortedd = sorted(unsorted,key = lambda x:x[1])
+# print(sortedd)
+
+# # sorting dictionaries in python
+# unsorted = {1:5,2:8,8:7,4:10,5:11}
+# sortedd = {k:v for k,v in sorted(unsorted.items(),key = lambda x:x[1])}
+# print(sortedd)
+
+# # zipping and unzipping
+# one = [1,2,3,4,5,6]
+# two = [6,5,4,3,2,1]
+# zipped = list(zip(one,two))
+# one,two = zip(*zipped)
+# one,two = list(one),list(two)
+# print(one,two)
+
+# # if loop doesn't break
+# for i in range(3):
+#     if i==3:
+#         break
+# else:
+#     print("loop didn't broke")
+
+# C++ upper bound and lower bound equivalent in python
+# using bisect module
+# https://docs.python.org/3/library/bisect.html
+# bisect_left returns i such that all(val < x for val in a[lo:i]) for left side and all(val >= x for val in a[i:hi]) for right side
+# bisect_right just reverse
+# bisect similar to bisectleft but val<=x instead of val<x
+# insort functions are just insert(returned i from above functions,x)
