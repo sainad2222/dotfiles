42597c49d75e982bf6f452da3d5dcfb21f795cb0
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2021-04-20 16:48:53 +0530

    updated kde script and lsp config

--- kde_setup.sh
+++ kde_setup.sh
@@ -2,3 +2,5 @@
 # https://store.kde.org/p/1166510/
 # 2 instances of this widget one for MEM: and one for %
 printf "%.1f" $(free | grep Mem | awk '{print $3/$2 * 100.0}' | bc -l) && echo %
+# for task-switcher visualization theme
+sudo apt-get install kwin-addons 
